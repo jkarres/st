aa08d8aa0e990e7775d13dae7337bfb62571408c
Jeffrey Karres <jkarres@gmail.com>
2019-03-22 00:19:44 -0700

    Monoid Nerd Font Mono -> Custom Iosevka

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monoid Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Iosevka Custom:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
